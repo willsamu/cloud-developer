5d3301f693d28e699423b1d683c87c1616bca145
willsamu <bok5@gmx.net>
2020-05-24 01:26:46 +0200

    Refactor: rm logs

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,7 +30,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   app.get("/filteredimage/", async (req: Request, res: Response) => {
     const { image_url } = req.query;
     if (!image_url) {
-      return res.status(400).send("Did not provide an imageUrl!");
+      return res.status(400).send("Did not provide an image_url!");
     }
     console.log("Url recieved: ", image_url);
     return filterImageFromURL(image_url)
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -10,9 +10,8 @@ import Jimp = require("jimp");
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    let photo = null;
     try {
-      photo = await Jimp.read(inputURL);
+      const photo = await Jimp.read(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
@@ -26,7 +25,6 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
       console.log("Error processing input url..");
       reject();
     }
-    // const photo = await Jimp.read(inputURL);
   });
 }
 
