3abbc73fec38e57bf19cb68aa992bee1d2263431
willsamu <bok5@gmx.net>
2020-05-23 23:50:34 +0200

    Feature: Add Request processing

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
@@ -8,6 +8,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   // Set the network port
   const port = process.env.PORT || 8082;
+  console.log("Port: ", process.env.PORT);
 
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -26,6 +27,18 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    const { image_url } = req.query;
+    if (!image_url) {
+      return res.status(400).send("Did not provide an imageUrl!");
+    }
+    console.log("Url recieved: ", image_url);
+    const temp_path = await filterImageFromURL(image_url);
+    console.log(`Got the converted image at ${temp_path}`);
+    res.status(200).sendFile(temp_path);
+    return res.on("finish", () => deleteLocalFiles([temp_path]));
+  });
+
   /**************************************************************************** */
 
   //! END @TODO1
@@ -33,7 +46,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // Root Endpoint
   // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}");
+    res.status(200).send("try GET /filteredimage?image_url={{}}");
   });
 
   // Start the Server
